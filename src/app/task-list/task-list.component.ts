c70ab1535a2a8d8557938c067063151ef8ad35f5
calado <jricardoc10@gmail.com>
2017-07-05 17:00:03 -0300

    first commit

---
+++ src/app/task-list/task-list.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Task } from "../task";
+
+@Component({
+  selector: 'task-list',
+  templateUrl: './task-list.component.html',
+  styleUrls: ['./task-list.component.css']
+})
+export class TaskListComponent {
+
+   title = 'Tarefas';
+  tasks=[];
+  task:Task={
+      name:"",
+      value:0
+
+  };
+
+
+  add(){
+    let task = Object.assign({},this.task);
+    this.tasks.push(task);
+  }
+}
